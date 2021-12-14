5560c262572f99d3b64bbb6d45027dd2365644bf
Kevin Ernest Long <kevinelong@gmail.com>
2021-12-14 08:34:40 +0000

    interactivity

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
-document.addEventListener("DOMContentLoaded",()=>{
+const toName = name => name.toLowerCase().replace(/[^a-zA-Z0-9À-ž\s]/g, "-").replace(" ","_");
 
-const get = q => document.querySelectorAll(q)[0];
+const select = e => {
+    const selected = e.parentElement.getElementsByClassName('selected');
+    [...e.parentElement.children].forEach(s => s.classList.remove('selected'));
+    e.classList.add('selected');
+    const b = document.body;
+    [...b.classList].forEach(c => b.classList.remove(c));
+    b.classList.add(`selected-${toName(e.parentElement.id)}-${toName(e.id)}`);
+}
 
-const div = (className, content) => `<div class="${className}">${content}</div>`
 
-const title = text => div("title", text)
-const choiceSet = content => div("choice-set", content)
-const choice = text => div("choice", text)
-const choiceSelected = text => div("choice selected", text)
+document.addEventListener("DOMContentLoaded", () => {
 
-const cardList = content => div("card-list", content)
-const card = text => div("card", text)
+    const get = q => document.querySelectorAll(q)[0];
 
-const listItem = text => div("nav-list-item", text)
+    const div = (className, content, attrs) => `<div class="${className}" ${attrs}>${content}</div>`
 
-mainNavTab = (text="", selected=false, icon="compass") => `<div class="main-nav-tab">
+    const title = text => div("title", text)
+    const choiceSet = content => div("choice-set", content, "id='secondary-nav'")
+    const choice = (text, selected=false) => div(`choice choice-${toName(text)} ${selected?'selected':''}`, text, `id='${toName(text)}' onclick='select(this)'`)
+
+    const cardList = content => div("card-list", content)
+    const card = text => div("card", text)
+
+    const listItem = text => div("nav-list-item", text)
+
+
+    mainNavTab = (text = "", selected = false, icon = "compass") => `<div id="main-nav-tab-${toName(text)}" class="main-nav-tab ${selected ? ' selected' : ''}" onclick="select(this)">
     <div class="main-nav-tab-background">
-        <div class="icon-frame ${selected?'selected':''}">
-            <img class="icon" src="./${icon}.svg">
+        <div class="icon-frame">
+            <img class="icon" src="./images/icon-nav-${icon}-white.svg">
         </div>
         <div class="label">${text}</div>
     </div>
 </div>`
 
 
-get(".main-nav").innerHTML = mainNavTab("EXPLORE") + 
-mainNavTab("BOARDS", false, "boards") + 
-mainNavTab("PEOPLE", true, "people") + 
-mainNavTab("AGENDA", false, "planner") + 
-mainNavTab("PAYMENTS",false, "payments") ;
+    get(".main-nav").innerHTML = mainNavTab("EXPLORE") +
+        mainNavTab("BOARDS", false, "boards") +
+        mainNavTab("PEOPLE", true, "people") +
+        mainNavTab("AGENDA", false, "planner") +
+        mainNavTab("PAYMENTS", false, "payments");
 
-get(".inner-content").innerHTML = title("PEOPLE (/)") + 
-choiceSet(
-    choiceSelected("ALL NETWORK (23)") + 
-    choice("PLANNERS (5)") + 
-    choice("FRIENDS (5)") + 
-    choice("CONFIRMATIONS (1)") +
-    choice("NEW PEOPLE (1)") +
-    choice("MATCHES (1)") +
-    choice("DEALS (1)")
-) + 
-// listItem("List Item 1") + listItem("List Item 2") +
-cardList(
-    card("[Kind] Subject<br>&nbsp;<br>Body of the text message.") +
-    card("[Kind] Subject<br>&nbsp;<br>Body of the text message."))
+    // select(get("#main-nav-tab-people"));
+
+    get(".inner-content").innerHTML = title("... PEOPLE (/) *") +
+        choiceSet(
+            choice("ALL NETWORK (23)", true) +
+            choice("PLANNERS (5)") +
+            choice("FRIENDS (5)") +
+            choice("CONFIRMATIONS (1)") +
+            choice("NEW PEOPLE (1)") +
+            choice("MATCHES (1)") +
+            choice("DEALS (1)")
+        ) +
+        // listItem("List Item 1") + listItem("List Item 2") +
+        cardList(
+            card("[Kind] Subject<br>&nbsp;<br>Body of the text message.") +
+            card("[Kind] Subject<br>&nbsp;<br>Body of the text message."))
 });
