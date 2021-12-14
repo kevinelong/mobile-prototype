6d84290613afd69aa338f263cf5fe97674b61325
kevinelong <kevinelong@gmail.com>
2021-12-14 11:51:30 -0800

    icons styles circles

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const toName = name => name.toLowerCase().replace(/[^a-zA-Z0-9À-ž\s]/g, "-").replace(" ","_");
-
 const select = e => {
     const selected = e.parentElement.getElementsByClassName('selected');
     [...e.parentElement.children].forEach(s => s.classList.remove('selected'));
@@ -8,43 +7,31 @@ const select = e => {
     [...b.classList].forEach(c => b.classList.remove(c));
     b.classList.add(`selected-${toName(e.parentElement.id)}-${toName(e.id)}`);
 }
-
-
 document.addEventListener("DOMContentLoaded", () => {
-
     const get = q => document.querySelectorAll(q)[0];
-
     const div = (className, content, attrs) => `<div class="${className}" ${attrs}>${content}</div>`
-
     const title = text => div("title", text)
+    const circle= text => div("circle", text)
     const choiceSet = content => div("choice-set", content, "id='secondary-nav'")
     const choice = (text, selected=false) => div(`choice choice-${toName(text)} ${selected?'selected':''}`, text, `id='${toName(text)}' onclick='select(this)'`)
-
     const cardList = content => div("card-list", content)
     const card = text => div("card", text)
-
+    const icon = (icon = "menu") => `<img class="icon" src="./images/icons/icon-${icon}.svg">`;
     const listItem = text => div("nav-list-item", text)
-
-
     mainNavTab = (text = "", selected = false, icon = "compass") => `<div id="main-nav-tab-${toName(text)}" class="main-nav-tab ${selected ? ' selected' : ''}" onclick="select(this)">
     <div class="main-nav-tab-background">
         <div class="icon-frame">
-            <img class="icon" src="./images/icon-nav-${icon}-white.svg">
+            <img class="icon" src="./images/nav/icon-nav-${icon}-white.svg">
         </div>
         <div class="label">${text}</div>
     </div>
 </div>`
-
-
     get(".main-nav").innerHTML = mainNavTab("EXPLORE") +
         mainNavTab("BOARDS", false, "boards") +
         mainNavTab("PEOPLE", true, "people") +
         mainNavTab("AGENDA", false, "planner") +
         mainNavTab("PAYMENTS", false, "payments");
-
-    // select(get("#main-nav-tab-people"));
-
-    get(".inner-content").innerHTML = title("... PEOPLE (/) *") +
+    get(".inner-content").innerHTML = title(icon("menu") + div("title-middle", "People&nbsp;&nbsp;" + circle(icon("search"))) + icon("account-circle") ) +
         choiceSet(
             choice("ALL NETWORK (23)", true) +
             choice("PLANNERS (5)") +
@@ -54,8 +41,8 @@ document.addEventListener("DOMContentLoaded", () => {
             choice("MATCHES (1)") +
             choice("DEALS (1)")
         ) +
-        // listItem("List Item 1") + listItem("List Item 2") +
         cardList(
             card("[Kind] Subject<br>&nbsp;<br>Body of the text message.") +
-            card("[Kind] Subject<br>&nbsp;<br>Body of the text message."))
+            card("[Kind] Subject<br>&nbsp;<br>Body of the text message.")
+        )
 });
