ef9822ff118904b39e01d928b43ea0004533859a
kevinelong <kevinelong@gmail.com>
2022-01-06 17:19:20 -0800

    refactor person icon and build icon paths

--- scripts/components/components.js
+++ scripts/components/components.js
@@ -1,11 +1,28 @@
-const icon = (icon = "menu", iconColor = "") => div("icon-frame", `<img class="icon" src="./images/icons/icon-${icon}${iconColor ? "-" + iconColor : ""}.svg">`);
-// const listItem = text => div("nav-list-item", text);
-const text = (text) => div("text", text);
+const text = textValue => div("text", textValue);
+const title = textValue => div("title", textValue);
+const subtitle = textValue => div("subtitle", textValue);
+const circle = textValue => div("circle", textValue);
+
 const contentPanel = content => content ? div("content-panel", content) : "";
 
-const title = text => div("title", text);
-const subtitle = text => div("subtitle", text);
-const circle = text => div("circle", text);
+const row = content => div("row", content);
+const col = content => div("col", content);
+// debugger;
+const PATH_STATIC = "";
+const PATH_IMAGES = `${PATH_STATIC}/images/`;
+const PATH_ICONS = `${PATH_IMAGES}/icons/`;
+const PREFIX_ICONS = `${PATH_ICONS}/icon-`;
+const SUFFIX_ICONS = `.svg`;
 
-const row = (c) => div("row", c);
-const col = (c) => div("col", c);
+const iconPath = (name, color = "") => [
+    PREFIX_ICONS,
+    name,
+    color ? "-" + color : "",
+    SUFFIX_ICONS
+].join('').replace(/([^:]\/)\/+/g, "$1");
+
+const icon = (icon = "menu", iconColor = "") => div("icon-frame",
+    `<img class="icon" src="${iconPath(icon, iconColor)}">`);
+
+const person = (iconName, textName = "") => actionItem("person", "", "",
+    textName ? textName : iconName);
--- scripts/components/search/search.js
+++ scripts/components/search/search.js
@@ -1,7 +1,7 @@
 const search = (items) =>
     label(
-        icon("search", "black") +
-        input("search", "text",
+        // icon("search", "black") +
+        input("vita-search", "text",
             "placeholder=\"Search Filter Text\""
             // +" onkeypress=\"if (this.value.length > 0){ show('.autocomplete'); }else{ hide('.autocomplete');}\""
         ) +
