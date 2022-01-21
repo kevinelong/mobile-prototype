7c45a828903797083bd3be894998ef075c508982
jvorvick <49791235+jvorvick@users.noreply.github.com>
2022-01-21 13:56:51 -0800

    added "taste match" Dream notification card

--- scripts/components/card/card_dream.js
+++ scripts/components/card/card_dream.js
@@ -1,20 +1,19 @@
-function dreamCardNotification(who, quantity, which) {
+function dreamCardNotification(
+    people,
+    quantity,
+    which,
+    content = "",
+    actions = []
+) {
     return card(
         "dream",
         div(
             "titles dream",
             row(icon("dream") + col(cardTitle("Dream") + cardSubtitle(which)))
-        ) +
-            actionItem(
-                "open",
-                "dream"
-                // "https://www.figma.com/proto/RNFPr2XMBBFuj60EEo3TK7/Vita---Greg?page-id=1%3A995&node-id=724%3A3890&viewport=241%2C48%2C0.45&scaling=min-zoom&starting-point-node-id=724%3A3890&show-proto-sidebar=0"
-            ),
-        text(
-            `${quantity} new items added to your linked ${which} dream board by your friend ${who.name}.`
-        ),
-        [who],
-        ["dream"],
+        ) + actionItem("open", "dream"),
+        content,
+        people,
+        actions,
         "",
         "",
         "",
