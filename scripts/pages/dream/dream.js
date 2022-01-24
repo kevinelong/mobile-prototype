a61ec2dc42bfe6423882e8d0e633867564444dbf
kevinelong <kevinelong@gmail.com>
2022-01-24 07:47:16 -0800

    add dreamer data to board list

--- scripts/pages/dream/dream.js
+++ scripts/pages/dream/dream.js
@@ -1,10 +1,10 @@
-function dreamCard(title, subtitle = "", actions = [], which = "", id = 0, image="") {
+function dreamCard(title, subtitle = "", actions = [], which = "", id = 0, image="", groups=[]) {
     const kind = "board"
     return card(
         kind,
         cardTitles(kind, title, subtitle, which, id),
         "",
-        [],
+        groups,
         actions,
         image
     );
@@ -19,9 +19,23 @@ function dreamPage(selected = false) {
         "All",
         cardList([
             dreamCard("Santa Barbara", "California", [], "dream_board", 1,
-                "images/photos/santa_barbara_1500x500.jpg"),
+                "images/photos/santa_barbara_1500x500.jpg",
+                [{
+                    people: peopleList,
+                    title: "Linked with",
+                    groupName: "Dreamer",
+                    subtitle: "23 cards shared"
+                }]
+            ),
             dreamCard("Paris", "France", [], "dream_board", 2,
-                "images/photos/paris_france.jpg"),
+                "images/photos/paris_france.jpg",
+                [{
+                    people: [peopleList[JOE]],
+                    title: "Linked with",
+                    groupName: "Dreamer",
+                    subtitle: "2 cards shared"
+                }]
+            ),
         ].join("")),
         "ALL NETWORK"
     );
