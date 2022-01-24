ad6213d58794b79cfff1e61698322b8b2c63b30f
kevinelong <kevinelong@gmail.com>
2022-01-24 01:42:39 -0800

    improve profile

--- scripts/route.js
+++ scripts/route.js
@@ -66,11 +66,37 @@ function showProfileDialog(target, action, which, index = RUBY) {
             )
             .join("")
     );
+    const other = text(
+            "Relationship" +
+            input("relationship", "text", `placeholder="e.g. Acquaintance, Friend, BFF"`)
+        ) +
+        text(
+            "Shared Plans" +
+            div("card-tags", [...[
+                "Brunch - Sunday March 3rd 2022"
+            ]].map(hashTag).join(""))
+        ) +
+        text(
+            "Interests" +
+            div("card-tags", [...[
+                "Skiing", "Fine Dining - Shared", "Wine Tasting - Shared"
+            ]].map(hashTag).join(""))
+        ) +
+        text(
+            "Boards" +
+            div("card-tags", [...[
+                "Santa Barbara - Linked", "Paris - Shared. Link?", "Seattle"
+            ]].map(hashTag).join(""))
+        ) +
+        ((index === 0) ? "" : actions) +
+        cardGroups(who.groups);
     const content = contentPanel(
-        person(who) + ((index === 0) ? "" : actions) + cardGroups(who.groups),
+        person(who) +
+        (index===0 ? "" : other)
+        ,
         "profile"
     );
-    showDialog(index === 0 ? "Profile" : "Connection Profile", content);
+    showDialog(index === 0 ? "Your Profile" : "Connection Profile", content);
 }
 
 function collapseCard(target) {
@@ -105,6 +131,7 @@ TOAST_MESSAGES = {
     paypal: "Payment Settled",
     venmo: "Payment Settled",
 };
+
 function addItem(target, action, which, id) {
     if ("person" === which) {
         showSearchDialog(id);
@@ -114,8 +141,9 @@ function addItem(target, action, which, id) {
         addMessage();
     }
 
-   //   console.log("add", ...arguments);
+    //   console.log("add", ...arguments);
 }
+
 ACTION_PAGES = {
     back: () => showPage(window.lastPage),
     open: openPage,
@@ -130,27 +158,29 @@ ACTION_PAGES = {
     person: showProfileDialog,
     match: showMatchDialog,
 };
+
 function toggleCollapse(target) {
     if (!target) {
-       //   console.log("toggleCollapse(...)", "required target is not defined.");
+        //   console.log("toggleCollapse(...)", "required target is not defined.");
         return false;
     }
     const card_list = target.closest(".card-list");
     if (!card_list) {
-       //   console.log("", "No ancestor .card-list");
+        //   console.log("", "No ancestor .card-list");
         return false;
     }
     card_list.classList.toggle("collapse");
 }
+
 function route(target, action, which = "", index = "") {
     if (TOAST_MESSAGES.hasOwnProperty(action)) {
         return showToast(TOAST_MESSAGES[action]);
     } else if (ACTION_PAGES.hasOwnProperty(action)) {
-       //   console.log("ACTION: " + action, which, index);
+        //   console.log("ACTION: " + action, which, index);
         const f = ACTION_PAGES[action];
-       //   console.log(f);
+        //   console.log(f);
         return f(target, action, which, index);
     } else {
-       //   console.log("UNKNOWN ACTION:" + action);
+        //   console.log("UNKNOWN ACTION:" + action);
     }
 }
