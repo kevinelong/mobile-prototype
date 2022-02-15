46de20fdb8bab54272a909daf5e5918069ec8444
kevinelong <kevinelong@gmail.com>
2022-02-15 12:32:13 -0800

    replace influencer

--- scripts/pages/explore/explore.js
+++ scripts/pages/explore/explore.js
@@ -13,7 +13,7 @@ const EXPLORE_DATA = [
         "images/photos/hanna-levin.png",
         "Hanna Levin",
         "New Taste Match to Follow!",
-        "Hanna Levin has rated over 20 places, including 2 you both love!", ["Influencer", "Recommended"],
+        "Hanna Levin has rated over 20 places, including 2 you both love!", ["Tapas", "Spanish"],
         [{
                 people: [peopleList[BF]],
                 title: "Recommended By",
