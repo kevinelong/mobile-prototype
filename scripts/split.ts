6156c350aee6442b947182bef26c6179531b9645
kevinelong <kevinelong@gmail.com>
2022-05-10 10:35:52 -0700

    adv split wip

--- scripts/split.ts
+++ scripts/split.ts
@@ -1,13 +1,17 @@
 'use strict';
-
+function r(amount:number):number{
+    return Math.round(amount*100)/100;
+}
 class Person {
     public name: string;
 
     constructor(name: string) {
         this.name = name;
     }
+    toString(){
+        return this.name;
+    }
 }
-
 class Group {
     public list: Array<Person>;
 
@@ -15,7 +19,6 @@ class Group {
         this.list = list;
     }
 }
-
 class Portion {
     public who: Person;
     public totalAmount: number;
@@ -23,6 +26,7 @@ class Portion {
     public paid: number;
     public portion: number; //agrees to pay
     public balance: number = 0; //Owes
+    public explicit: boolean = false; //Dirty flag if this person's share has been customized for this transaction. true==dirty
 
     constructor(who: Person, totalAmount = 0, percent = 0, paid = 0) {
         this.who = who;
@@ -37,20 +41,30 @@ class Portion {
     }
     setPaidAmount(amount:number){
         this.paid = amount;
+        this.explicit = true;
         this.updateBalance();
     }
+    setPortion(portion:number){
+        this.portion = portion;
+        this.explicit = true;
+        this.updateBalance();
+    }
+    toString():string{
+        return `\n\t\t${this.who} ${r(this.totalAmount)} ${r(this.percent)}  ${r(this.paid)} ${r(this.portion)} ${r(this.balance)}`;
+    }
 }
-
 class Expense {
     public name: string;
     public amount: number;
     public group: Array<Person>;
     public portionList: Array<Portion> = [];
+    public turn: number;
 
-    constructor(name: string, amount: number, group: Array<Person>) {
+    constructor(name: string, amount: number, group: Array<Person>, turn= 0) {
         this.name = name;
         this.amount = amount;
         this.group = group;
+        this.turn = turn;
         group.forEach(
             p => this.portionList.push(
                 new Portion(
@@ -60,19 +74,54 @@ class Expense {
                     0
                 )
             )
-        )
+        );
+        this.makeBalance();
     }
+    makeBalance(){
+        let total = 0;
+        this.portionList.map(p => total += p.portion);
+        const old = this.portionList[this.turn].portion;
+        const pennies = this.amount - total;
+        this.portionList[this.turn].setPortion(pennies + old);
+    }
+
     setPaidAmount(who_index:number, amount:number){
         this.portionList[who_index].setPaidAmount(amount);
+        this.makeBalance();
     }
-}
 
+    toString():string{
+        return `NAME:${this.name}\n\tAMOUNT:${this.amount}\n\tPORTION_LIST:${this.portionList.map(e=>e.toString()).join('\n\t')}`;
+    }
+
+}
+/* BEGIN TESTS */
 let g:Array<Person> = [
     new Person("KL"),
     new Person("NM"),
     new Person("PD")
 ];
 
-let e = new Expense("Lunch", 10.00, g);
+let e = new Expense(
+    "Lunch",
+    10.00,
+    g,
+    0);
+console.log(e+'');
+
+console.assert(
+    r(e.portionList[0].balance) === -3.34,
+    "",
+    `BALANCE: ${
+        r(e.portionList[0].balance)
+    } should be ${
+        -3.34
+    }`);
+
 e.portionList[0].setPaidAmount(10.00);
-console.log(e.portionList);
+
+console.assert(
+    r(e.portionList[0].balance) === 6.66
+);
+
+console.log(e+'');
