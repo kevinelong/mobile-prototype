1207bde110113082a35798d25edb208e622e7ad8
kevinelong <kevinelong@gmail.com>
2022-06-03 07:05:40 -0700

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// noinspection JSUnresolvedVariable
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "plugins": [
+        "html"
+    ]
+}
